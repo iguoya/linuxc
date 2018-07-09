45c85db9ef9b41b81658ee2b2fe06b8409fd70ed
Snow Tiger <375478250@qq.com>
2018-07-10 00:47:20 +0800

    修正误删除

--- superpipe/main.c
+++ superpipe/main.c
@@ -19,6 +19,10 @@ int get_prime_number(int n) {
             } else {
                 continue;
             }
+        }
+//        printf("Now I test is_prime_number: %d \n", is_prime_number);
+        if (flag == 1) {
+            return is_prime_number;
         } else {
             return 0;
         }
